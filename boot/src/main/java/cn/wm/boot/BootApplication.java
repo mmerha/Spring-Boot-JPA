649b190c22ec1c63d68a2d6f342e2a6b1c4c1393
Administrator <Administrator@192.168.0.105>
2019-05-21 11:56:06 +0800

    SpringBoot和JPA结合的框架，简化了SSM框架的一些配置文件

---
+++ boot/src/main/java/cn/wm/boot/BootApplication.java
@@ -0,0 +1,12 @@
+package cn.wm.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//SpringBoot框架启动类
+@SpringBootApplication
+public class BootApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(BootApplication.class, args);
+	}
+}
